0949341405929ee84a3e032bcd144db8c874e008
Petr Vorlíček <petrvorlicek7@gmail.com>
2023-05-06 14:29:19 +0200

    nejaky updates

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -4,35 +4,54 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-     public float forceMultiplier = 10f; // The amount of force applied to the player when dragged
-    public LineRenderer directionLine; // A line renderer to display the direction the player will go
+    public float forceMultiplier = 10f;
+    public float stopThreshold = 1f;
+    public float damping = 0.5f;
+    public LineRenderer directionLine;
 
-    private Vector2 dragStartPos; // The starting position of the player drag
-    private Rigidbody2D rb; // The player's Rigidbody2D component
+    private Vector2 dragStartPos;
+    private Rigidbody2D rb;
+    private bool isDragging = false;
+    private float lastVelocity = 0f;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        directionLine.enabled = false; // Disable the direction line initially
+        directionLine.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (!isDragging && rb.velocity.magnitude < stopThreshold)
+        {
+            rb.velocity *= damping;
+            if (Mathf.Abs(rb.velocity.magnitude - lastVelocity) < 0.01f)
+            {
+                rb.velocity = Vector2.zero;
+            }
+            lastVelocity = rb.velocity.magnitude;
+        }
     }
 
     private void OnMouseDown()
     {
         dragStartPos = transform.position;
-        directionLine.enabled = true; // Enable the direction line when the player is clicked
+        directionLine.enabled = true;
+        isDragging = true;
     }
 
     private void OnMouseDrag()
     {
         Vector2 dragDelta = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position) * -1f;
         directionLine.SetPosition(0, transform.position);
-        directionLine.SetPosition(1, transform.position + (Vector3)dragDelta.normalized * 2f); // The line will be 2 units long
+        directionLine.SetPosition(1, transform.position + (Vector3)dragDelta.normalized * 2f);
     }
 
     private void OnMouseUp()
     {
         Vector2 dragDelta = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position) * -1f;
         rb.AddForce(dragDelta * forceMultiplier, ForceMode2D.Force);
-        directionLine.enabled = false; // Disable the direction line when the player is released
+        directionLine.enabled = false;
+        isDragging = false;
     }
 }
