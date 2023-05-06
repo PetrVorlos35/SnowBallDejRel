8f662a3a5f9aa98355c84d290ff7d0cfe9b8d047
Dejnyyy <danslaby@email.cz>
2023-05-06 23:55:22 +0200

    zvetsuje se nam kuli
    mozna moc ale jeste to poresim

---
+++ Assets/scripts/SnowBallScalling.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowBallScalling : MonoBehaviour
+{
+    public GameObject snowballPrefab;
+    public float baseGrowthRate = 0.1f;
+    GameObject snowball;
+    Coroutine growCoroutine;
+
+    private void Start()
+    {
+        snowball = Instantiate(snowballPrefab, transform.position, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        // Check if the snowball is moving
+        if (snowball.GetComponent<Rigidbody2D>().velocity.magnitude > 0f)
+        {
+            // If the snowball is moving and the coroutine is not already running, start growing the snowball
+            if (growCoroutine == null)
+            {
+                growCoroutine = StartCoroutine(GrowSnowball(snowball));
+            }
+        }
+        else
+        {
+            // If the snowball is not moving and the coroutine is running, stop growing the snowball
+            if (growCoroutine != null)
+            {
+                StopCoroutine(growCoroutine);
+                growCoroutine = null;
+            }
+        }
+    }
+
+    IEnumerator GrowSnowball(GameObject snowball)
+    {
+        while (snowball.transform.localScale.x < 10f)
+        {
+            float growthRate = baseGrowthRate / snowball.transform.localScale.x;
+            snowball.transform.localScale += new Vector3(growthRate, growthRate, 0f) * 0.5f;
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+}
